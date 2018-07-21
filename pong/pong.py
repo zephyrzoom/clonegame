02ad9a8bc0601213090ef67f6ba6208c0e6972f4
707 <707472783@qq.com>
2018-07-21 14:25:54 +0800

    add racket control

--- pong/pong.py
+++ pong/pong.py
@@ -2,62 +2,69 @@ import pyglet
 from pyglet.window import key
 from pyglet.window import mouse
 
+class Window(pyglet.window.Window):
 
-window = pyglet.window.Window()
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.A:
-        print('The "A" key was pressed.')
-    elif symbol == key.LEFT:
-        print('The left arrow key was pressed.')
-    elif symbol == key.ENTER:
-        print('The enter key was pressed.')
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+
+        self.title = pyglet.text.Label('Pong',
+                            font_name='Times New Roman',
+                            font_size=28,
+                            x=self.width//2, y=self.height-20,
+                            anchor_x='center', anchor_y='center')
+        
+        pyglet.resource.path = ['assets']
+        pyglet.resource.reindex()
+        self.image = pyglet.resource.image('ball.png')
+        self.center_image(self.image)
+        self.music = pyglet.resource.media('rebound.ogg')
+        self.music.play()
+        self.ball = pyglet.sprite.Sprite(img=self.image, x=500, y=400)
+        self.ball.scale = 0.2
+        self.ball.dx = 10.0
+        self.racket_image = pyglet.resource.image('racket.png')
+        self.racket_left = pyglet.sprite.Sprite(img=self.racket_image, x=50, y=50)
+        self.racket_left.scale = 0.5
+        self.racket_right = pyglet.sprite.Sprite(img=self.racket_image, x=self.width-50, y=50)
+        self.racket_right.scale = 0.5
+        self.fps_display = pyglet.clock.ClockDisplay()
+        pyglet.clock.schedule_interval(self.update, 1/60.0)
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.W:
+            pyglet.clock.schedule_interval(self.move, 5, self.racket_left)
+        elif symbol == key.S:
+            self.move(-10, self.racket_left)
+        elif symbol == key.UP:
+            self.move(10, self.racket_right)
+        elif symbol == key.DOWN:
+            self.move(-10, self.racket_right)
 
 
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    if button == mouse.LEFT:
-        print('The left mouse button was pressed.')
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == mouse.LEFT:
+            print('The left mouse button was pressed.')
 
 
-@window.event
-def on_draw():
-    window.clear()
-    title.draw()
-    ball.draw()
-    racket_left.draw()
-    racket_right.draw()
-    fps_display.draw()
+    def on_draw(self):
+        self.clear()
+        self.title.draw()
+        self.ball.draw()
+        self.racket_left.draw()
+        self.racket_right.draw()
+        self.fps_display.draw()
 
-def center_image(image):
-    """Sets an image's anchor point to its center"""
-    image.anchor_x = image.width // 2
-    image.anchor_y = image.height // 2
+    def center_image(self, image):
+        """Sets an image's anchor point to its center"""
+        image.anchor_x = image.width // 2
+        image.anchor_y = image.height // 2
 
-def update(dt, sprite):
-    sprite.x += sprite.dx * dt
+    def update(self, dt):
+        self.ball.x += self.ball.dx * dt
+
+    def move(self, dt, sprite):
+        sprite.y += dt
 
 if __name__ == '__main__':
-
-    title = pyglet.text.Label('Pong',
-                          font_name='Times New Roman',
-                          font_size=28,
-                          x=window.width//2, y=window.height-20,
-                          anchor_x='center', anchor_y='center')
-    pyglet.resource.path = ['assets']
-    pyglet.resource.reindex()
-    image = pyglet.resource.image('ball.png')
-    center_image(image)
-    music = pyglet.resource.media('rebound.ogg')
-    music.play()
-    ball = pyglet.sprite.Sprite(img=image, x=500, y=400)
-    ball.scale = 0.2
-    ball.dx = 10.0
-    racket_image = pyglet.resource.image('racket.png')
-    racket_left = pyglet.sprite.Sprite(img=racket_image, x=50, y=50)
-    racket_left.scale = 0.5
-    racket_right = pyglet.sprite.Sprite(img=racket_image, x=window.width-50, y=50)
-    racket_right.scale = 0.5
-    fps_display = pyglet.clock.ClockDisplay()
-    pyglet.clock.schedule_interval(update, 1/60.0, sprite=ball)
+    window = Window()
     pyglet.app.run()

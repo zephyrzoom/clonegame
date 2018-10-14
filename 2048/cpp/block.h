212dc77398b78c016a4c1c8912f54904a24cf7ce
707 <707472783@qq.com>
2018-10-14 22:25:38 +0800

    update scene add some public method

--- 2048/cpp/block.h
+++ 2048/cpp/block.h
@@ -3,13 +3,13 @@
 
 class Block {
     private:
-    int _width;
-    int _x;
-    int _y;
-    int _value;
+        int _width;
+        int _x;
+        int _y;
+        int _value;
     public:
-    Block(int width, int x, int y, int value);
-    void move(int to_x, int to_y);
+        Block(int width, int x, int y, int value);
+        void move(int to_x, int to_y, int speed);
 };
 
 #endif
--- 2048/cpp/scene.h
+++ 2048/cpp/scene.h
@@ -1,6 +1,23 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include "block.h"
+#include <vector>
+
 class Scene {
     private:
-    int height;
-    int weight;
-    
+        int _height;
+        int _weight;
+
+        std::vector<Block> _blocks;
+
+    public:
+        Scene();
+
+        void move_left();
+        void move_right();
+        void move_up();
+        void move_down();
 };
+
+#endif __SCENE_H__
